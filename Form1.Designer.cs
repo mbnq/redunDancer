2c7369744b1f400cfbc208c7317c20572820aa45
mbnq <mbnq00@gmail.com>
2024-10-08 15:11:57 +0200

    designed starting gui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,301 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            mbLabelB = new Label();
+            mbIPTextBoxA = new TextBox();
+            mbMaskTextBoxA = new TextBox();
+            mbGatewayTextBoxA = new TextBox();
+            mbIPTextBoxB = new TextBox();
+            mbMaskTextBoxB = new TextBox();
+            mbGatewayTextBoxB = new TextBox();
+            mbDNS1TextBox = new TextBox();
+            mbDNS2TextBox = new TextBox();
+            IPLabel = new Label();
+            MaskLabel = new Label();
+            GatewayLabel = new Label();
+            DNS1Label = new Label();
+            DNS2Label = new Label();
+            ActiveACheckBox = new CheckBox();
+            ActiveBCheckBox = new CheckBox();
+            mbMaxPingTextBox = new TextBox();
+            MaxPingLabel = new Label();
+            textBox1 = new TextBox();
+            mbPingLogLabel = new Label();
+            mbStartPingMonitorButton = new Button();
+            mbStopPingMonitorButton = new Button();
+            mbButtonRun = new Button();
+            mbButtonStop = new Button();
+            SuspendLayout();
+            // 
+            // mbLabelB
+            // 
+            mbLabelB.AutoSize = true;
+            mbLabelB.Location = new Point(925, 15);
+            mbLabelB.Name = "mbLabelB";
+            mbLabelB.Size = new Size(18, 20);
+            mbLabelB.TabIndex = 0;
+            mbLabelB.Text = "B";
+            // 
+            // mbIPTextBoxA
+            // 
+            mbIPTextBoxA.Location = new Point(12, 48);
+            mbIPTextBoxA.Name = "mbIPTextBoxA";
+            mbIPTextBoxA.Size = new Size(198, 27);
+            mbIPTextBoxA.TabIndex = 1;
+            mbIPTextBoxA.Text = "192.168.4.1";
+            // 
+            // mbMaskTextBoxA
+            // 
+            mbMaskTextBoxA.Location = new Point(12, 81);
+            mbMaskTextBoxA.Name = "mbMaskTextBoxA";
+            mbMaskTextBoxA.Size = new Size(198, 27);
+            mbMaskTextBoxA.TabIndex = 1;
+            mbMaskTextBoxA.Text = "255.255.255.0";
+            // 
+            // mbGatewayTextBoxA
+            // 
+            mbGatewayTextBoxA.Location = new Point(12, 114);
+            mbGatewayTextBoxA.Name = "mbGatewayTextBoxA";
+            mbGatewayTextBoxA.Size = new Size(198, 27);
+            mbGatewayTextBoxA.TabIndex = 1;
+            mbGatewayTextBoxA.Text = "192.168.4.1";
+            // 
+            // mbIPTextBoxB
+            // 
+            mbIPTextBoxB.Location = new Point(381, 51);
+            mbIPTextBoxB.Name = "mbIPTextBoxB";
+            mbIPTextBoxB.Size = new Size(198, 27);
+            mbIPTextBoxB.TabIndex = 1;
+            mbIPTextBoxB.Text = "192.168.1.16";
+            // 
+            // mbMaskTextBoxB
+            // 
+            mbMaskTextBoxB.Location = new Point(381, 84);
+            mbMaskTextBoxB.Name = "mbMaskTextBoxB";
+            mbMaskTextBoxB.Size = new Size(198, 27);
+            mbMaskTextBoxB.TabIndex = 1;
+            mbMaskTextBoxB.Text = "255.255.255.0";
+            // 
+            // mbGatewayTextBoxB
+            // 
+            mbGatewayTextBoxB.Location = new Point(381, 117);
+            mbGatewayTextBoxB.Name = "mbGatewayTextBoxB";
+            mbGatewayTextBoxB.Size = new Size(198, 27);
+            mbGatewayTextBoxB.TabIndex = 1;
+            mbGatewayTextBoxB.Text = "192.168.1.1";
+            // 
+            // mbDNS1TextBox
+            // 
+            mbDNS1TextBox.Location = new Point(223, 198);
+            mbDNS1TextBox.Name = "mbDNS1TextBox";
+            mbDNS1TextBox.Size = new Size(168, 27);
+            mbDNS1TextBox.TabIndex = 2;
+            mbDNS1TextBox.Text = "8.8.8.8";
+            // 
+            // mbDNS2TextBox
+            // 
+            mbDNS2TextBox.Location = new Point(223, 231);
+            mbDNS2TextBox.Name = "mbDNS2TextBox";
+            mbDNS2TextBox.Size = new Size(168, 27);
+            mbDNS2TextBox.TabIndex = 2;
+            mbDNS2TextBox.Text = "8.8.4.4";
+            // 
+            // IPLabel
+            // 
+            IPLabel.AutoSize = true;
+            IPLabel.Location = new Point(234, 54);
+            IPLabel.Name = "IPLabel";
+            IPLabel.Size = new Size(118, 20);
+            IPLabel.TabIndex = 3;
+            IPLabel.Text = "<- IP Address ->";
+            // 
+            // MaskLabel
+            // 
+            MaskLabel.AutoSize = true;
+            MaskLabel.Location = new Point(227, 86);
+            MaskLabel.Name = "MaskLabel";
+            MaskLabel.Size = new Size(133, 20);
+            MaskLabel.TabIndex = 3;
+            MaskLabel.Text = "<- Subnet Mask ->";
+            // 
+            // GatewayLabel
+            // 
+            GatewayLabel.AutoSize = true;
+            GatewayLabel.Location = new Point(216, 117);
+            GatewayLabel.Name = "GatewayLabel";
+            GatewayLabel.Size = new Size(159, 20);
+            GatewayLabel.TabIndex = 3;
+            GatewayLabel.Text = "<- Default Gateway ->";
+            // 
+            // DNS1Label
+            // 
+            DNS1Label.AutoSize = true;
+            DNS1Label.Location = new Point(148, 201);
+            DNS1Label.Name = "DNS1Label";
+            DNS1Label.Size = new Size(47, 20);
+            DNS1Label.TabIndex = 3;
+            DNS1Label.Text = "DNS1";
+            // 
+            // DNS2Label
+            // 
+            DNS2Label.AutoSize = true;
+            DNS2Label.Location = new Point(148, 234);
+            DNS2Label.Name = "DNS2Label";
+            DNS2Label.Size = new Size(47, 20);
+            DNS2Label.TabIndex = 3;
+            DNS2Label.Text = "DNS2";
+            // 
+            // ActiveACheckBox
+            // 
+            ActiveACheckBox.AutoSize = true;
+            ActiveACheckBox.Checked = true;
+            ActiveACheckBox.CheckState = CheckState.Checked;
+            ActiveACheckBox.Location = new Point(12, 15);
+            ActiveACheckBox.Name = "ActiveACheckBox";
+            ActiveACheckBox.Size = new Size(41, 24);
+            ActiveACheckBox.TabIndex = 4;
+            ActiveACheckBox.Text = "A";
+            ActiveACheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ActiveBCheckBox
+            // 
+            ActiveBCheckBox.AutoSize = true;
+            ActiveBCheckBox.Location = new Point(538, 21);
+            ActiveBCheckBox.Name = "ActiveBCheckBox";
+            ActiveBCheckBox.Size = new Size(40, 24);
+            ActiveBCheckBox.TabIndex = 4;
+            ActiveBCheckBox.Text = "B";
+            ActiveBCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // mbMaxPingTextBox
+            // 
+            mbMaxPingTextBox.Location = new Point(223, 265);
+            mbMaxPingTextBox.Name = "mbMaxPingTextBox";
+            mbMaxPingTextBox.Size = new Size(168, 27);
+            mbMaxPingTextBox.TabIndex = 2;
+            mbMaxPingTextBox.Text = "100";
+            // 
+            // MaxPingLabel
+            // 
+            MaxPingLabel.AutoSize = true;
+            MaxPingLabel.Location = new Point(147, 268);
+            MaxPingLabel.Name = "MaxPingLabel";
+            MaxPingLabel.Size = new Size(70, 20);
+            MaxPingLabel.TabIndex = 3;
+            MaxPingLabel.Text = "Max Ping";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(612, 48);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(332, 375);
+            textBox1.TabIndex = 5;
+            // 
+            // mbPingLogLabel
+            // 
+            mbPingLogLabel.AutoSize = true;
+            mbPingLogLabel.Location = new Point(612, 25);
+            mbPingLogLabel.Name = "mbPingLogLabel";
+            mbPingLogLabel.Size = new Size(95, 20);
+            mbPingLogLabel.TabIndex = 3;
+            mbPingLogLabel.Text = "Ping Monitor";
+            // 
+            // mbStartPingMonitorButton
+            // 
+            mbStartPingMonitorButton.Location = new Point(613, 431);
+            mbStartPingMonitorButton.Name = "mbStartPingMonitorButton";
+            mbStartPingMonitorButton.Size = new Size(127, 29);
+            mbStartPingMonitorButton.TabIndex = 6;
+            mbStartPingMonitorButton.Text = "Start Monitor";
+            mbStartPingMonitorButton.UseVisualStyleBackColor = true;
+            // 
+            // mbStopPingMonitorButton
+            // 
+            mbStopPingMonitorButton.Location = new Point(816, 429);
+            mbStopPingMonitorButton.Name = "mbStopPingMonitorButton";
+            mbStopPingMonitorButton.Size = new Size(127, 29);
+            mbStopPingMonitorButton.TabIndex = 6;
+            mbStopPingMonitorButton.Text = "Stop Monitor";
+            mbStopPingMonitorButton.UseVisualStyleBackColor = true;
+            // 
+            // mbButtonRun
+            // 
+            mbButtonRun.Location = new Point(191, 317);
+            mbButtonRun.Name = "mbButtonRun";
+            mbButtonRun.Size = new Size(94, 29);
+            mbButtonRun.TabIndex = 7;
+            mbButtonRun.Text = "Run";
+            mbButtonRun.UseVisualStyleBackColor = true;
+            // 
+            // mbButtonStop
+            // 
+            mbButtonStop.Location = new Point(313, 317);
+            mbButtonStop.Name = "mbButtonStop";
+            mbButtonStop.Size = new Size(94, 29);
+            mbButtonStop.TabIndex = 7;
+            mbButtonStop.Text = "Stop";
+            mbButtonStop.UseVisualStyleBackColor = true;
+            // 
+            // mbForm1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(956, 475);
+            Controls.Add(mbButtonStop);
+            Controls.Add(mbButtonRun);
+            Controls.Add(mbStopPingMonitorButton);
+            Controls.Add(mbStartPingMonitorButton);
+            Controls.Add(textBox1);
+            Controls.Add(ActiveBCheckBox);
+            Controls.Add(ActiveACheckBox);
+            Controls.Add(mbPingLogLabel);
+            Controls.Add(MaxPingLabel);
+            Controls.Add(DNS2Label);
+            Controls.Add(DNS1Label);
+            Controls.Add(GatewayLabel);
+            Controls.Add(MaskLabel);
+            Controls.Add(IPLabel);
+            Controls.Add(mbMaxPingTextBox);
+            Controls.Add(mbDNS2TextBox);
+            Controls.Add(mbDNS1TextBox);
+            Controls.Add(mbGatewayTextBoxB);
+            Controls.Add(mbGatewayTextBoxA);
+            Controls.Add(mbMaskTextBoxB);
+            Controls.Add(mbMaskTextBoxA);
+            Controls.Add(mbIPTextBoxB);
+            Controls.Add(mbIPTextBoxA);
+            Controls.Add(mbLabelB);
+            Name = "mbForm1";
+            Text = "redunDancer";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label mbLabelB;
+        private TextBox mbIPTextBoxA;
+        private TextBox mbMaskTextBoxA;
+        private TextBox mbGatewayTextBoxA;
+        private TextBox mbIPTextBoxB;
+        private TextBox mbMaskTextBoxB;
+        private TextBox mbGatewayTextBoxB;
+        private TextBox mbDNS1TextBox;
+        private TextBox mbDNS2TextBox;
+        private Label IPLabel;
+        private Label MaskLabel;
+        private Label GatewayLabel;
+        private Label DNS1Label;
+        private Label DNS2Label;
+        private CheckBox ActiveACheckBox;
+        private CheckBox ActiveBCheckBox;
+        private TextBox mbMaxPingTextBox;
+        private Label MaxPingLabel;
+        private TextBox textBox1;
+        private Label mbPingLogLabel;
+        private Button mbStartPingMonitorButton;
+        private Button mbStopPingMonitorButton;
+        private Button mbButtonRun;
+        private Button mbButtonStop;
     }
 }
