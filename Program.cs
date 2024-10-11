6d06abb90fe573cebc3cc7dd86998a275be13ab7
mbnq <mbnq00@gmail.com>
2024-10-11 12:04:47 +0200

    0.0.0.7

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace redunDancer
         static Mutex redunDancerMutex = new Mutex(true, "{readunDancer}");
 
         #region variables and constants
-        public const string mbVersion = "0.0.0.4";
+        public const string mbVersion = "0.0.0.7";
         #endregion
 
         #region DPI
